e184e756a6fdabdf75443d8df845539f6df39382
ashirwadexe <ashirwadbappy@gmail.com>
2026-02-22 10:50:34 +0530

    node setup and db connection done

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+
+// forcing nodejs to use google or cloudflare's dns server
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is live...');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`App is running on PORT: ${PORT}`);
+    connectDB();
+});
+
